c3fce9ba916cea7be701347499509df3e7e54b23
mozrat <mozrat@gmail.com>
2010-03-23 09:18:44 +0000

    Adding a URL and function to delete (or rather unlink in the database) a Deliverable

--- django-project-management/deliverables/urls.py
+++ django-project-management/deliverables/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import *
 urlpatterns = patterns('deliverables.views',
 	(r'(?P<project_number>[-\w\./\s]+)/Add/$', 'add_deliverable'),
 	(r'(?P<project_number>[-\w\./\s]+)/(?P<deliverable_id>[-\w\./\s]+)/Edit/$', 'edit_deliverable'),
+	(r'(?P<project_number>[-\w\./\s]+)/(?P<deliverable_id>[-\w\./\s]+)/Delete/$', 'delete_deliverable'),
 	(r'(?P<project_number>[-\w\./\s]+)/(?P<deliverable_id>[-\w\./\s]+)/$', 'view_deliverable'),
 	(r'(?P<project_number>[-\w\./\s]+)/$', 'view_deliverables'),
 )
