7c900544b93496a1bd037dc53a1fb112d823d328
mozrat@gmail.com <mozrat@gmail.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-03-25 07:04:28 +0000

    Worked on ExtJS integration with the WBS application

--- trunk/django-project-management/wbs/admin.py
+++ trunk/django-project-management/wbs/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 #admin.site.register(WorkItem)
 #admin.site.register(ProjectPhase)
-#admin.site.register(SkillSet)
+admin.site.register(SkillSet)
 admin.site.register(EngineeringDay)
 
