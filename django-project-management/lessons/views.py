540bf3c387b356b3b928bcdb05fbfee5d7cde15a
mozrat <mozrat@gmail.com>
2010-03-25 08:44:08 +0000

    Fixed Lesson Learnt ExtJS forms

--- django-project-management/lessons/views.py
+++ django-project-management/lessons/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import simplejson as json
+
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,7 +12,7 @@ from django.db.models import Q
 from projects.models import *
 from projects.views import updateLog
 from lessons.forms import *
-from projects.misc import check_project_read_acl, check_project_write_acl
+from projects.misc import handle_form_errors, check_project_read_acl, check_project_write_acl, return_json_success
 
 
 @login_required
@@ -27,9 +29,9 @@ def add_lesson(request, project_number):
 			project.save()
 			request.user.message_set.create(message='''Lesson %s Registered''' % t.id)
 			updateLog(request, project_number, '''Lesson %s Registered''' % t.id)
-			return HttpResponseRedirect(project.get_absolute_url())
+			return HttpResponse( return_json_success() )
 		else:
-			pass
+			return HttpResponse( handle_form_errors(form.errors))
 
 @login_required
 def edit_lesson(request, project_number, lesson_id):
@@ -44,10 +46,9 @@ def edit_lesson(request, project_number, lesson_id):
 			request.user.message_set.create(message='''Lesson %s Edited''' % t.id)
 			for change in form.changed_data:
 				updateLog(request, project_number, '%s changed to %s' % ( change, eval('''t.%s''' % change)))
-			return HttpResponseRedirect(project.get_absolute_url())
+			return HttpResponse( return_json_success() )
 		else:
-			pass
-
+			return HttpResponse( handle_form_errors(form.errors))
 
 
 @login_required
@@ -68,4 +69,17 @@ def view_lessons(request, project_number):
 	check_project_read_acl(project, request.user)	# Will return Http404 if user isn't allowed to view project
 
 	return HttpResponse( serializers.serialize('json', project.lessons_learnt.all(), relations=('author',)))
+	
+@login_required
+def view_lesson(request, project_number, lesson_id):
+	project = Project.objects.get(project_number=project_number)
+	check_project_read_acl(project, request.user)	# Will return Http404 if user isn't allowed to view project
+	lesson = LessonLearnt.objects.get(id=lesson_id)
+
+	JSONSerializer = serializers.get_serializer('json')
+	j = JSONSerializer()
+	j.serialize([lesson], fields=('description', 'publish_to_client'))
+	
+	return HttpResponse( '''{ success: true, data: %s }''' % json.dumps(j.objects[0]['fields']))
+	return HttpResponse( serializers.serialize('json', LessonLearnt.objects.filter(id=lesson_id)))
 	
