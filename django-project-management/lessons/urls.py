4321cdb5c788d6ba63fa8735541f44f78a4e03cf
mozrat <mozrat@gmail.com>
2010-03-26 15:35:09 +0000

    Fixed deleting lessons

--- django-project-management/lessons/urls.py
+++ django-project-management/lessons/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import *
 
 urlpatterns = patterns('lessons.views',
 	(r'(?P<project_number>[-\w\./\s]+)/(?P<lesson_id>[-\w\./\s]+)/Edit/$', 'edit_lesson'),
+	(r'(?P<project_number>[-\w\./\s]+)/(?P<lesson_id>[-\w\./\s]+)/Delete/$', 'delete_lesson'),
 	(r'(?P<project_number>[-\w\./\s]+)/Add/$', 'add_lesson'),
 	(r'(?P<project_number>[-\w\./\s]+)/(?P<lesson_id>[-\w\./\s]+)/$', 'view_lesson'),
 	(r'(?P<project_number>[-\w\./\s]+)/$', 'view_lessons'),
