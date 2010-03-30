95442465b4a397a2bd72a0018902d690d1d171f6
mail <mail@johnbullonline.com>
2010-03-30 15:19:50 +0000

    Statemanagement added, date format changed on DatePickers

--- django-project-management/new_site_media/js/main-ui.js
+++ django-project-management/new_site_media/js/main-ui.js
@@ -1,17 +1,68 @@
+Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+
+Ext.namespace('Ext.ux.tree');
+Ext.ux.tree.State = function(config) {
+   Ext.apply(this, config);
+};
+Ext.extend(Ext.ux.tree.State, Ext.util.Observable, {
+   init: function(tree) {
+      this.tree = tree;
+      this.stateName = 'TreePanelState_' + this.tree.id;
+      this.idField = this.idField || 'id';
+      this.provider = Ext.state.Manager.getProvider() || new Ext.state.CookieProvider();
+      this.state = this.provider.get(this.stateName, {});
+      this.tree.on({
+         scope: this,
+         collapsenode: this.onCollapse,
+         expandnode: this.onExpand,
+         append: this.onNodeAdded
+      });
+   },
+    
+   saveState : function(newState) {
+      this.state = newState || this.state;
+      this.provider.set(this.stateName, this.state);
+   },
+    
+   onNodeAdded: function(tree, parentNode, node, index) {	
+      switch (this.state[node.id]) {
+         case 'C': 
+            node.expanded = false; 
+            break;
+         case 'E': 
+            node.expanded = true; 
+            break;
+      };
+   },
+   
+   onExpand: function(node) {
+      this.state[node.id] = 'E';
+      this.saveState();
+   },
+   
+   onCollapse: function(node) {
+      this.state[node.id] = 'C';
+      this.saveState();
+   }
+
+});
+
 var tree_data = [
-					{ text: 'Projects', leaf: false,
-						children: [{ text: 'View Dashboard', href: "/", leaf: true }, { text: "All Projects", href: "/AllProjects", leaf: true }] },
-  					{ text: 'Work In Progress', leaf: false,
+					{ text: 'Projects', id: 'projectsNode', leaf: false,
+						children: [{ text: 'View Dashboard', href: "/", leaf: true }, { text: "All Projects", href: "/AllProjects", leaf: true }]},
+  					{ text: 'Work In Progress', id: 'wipNode', leaf: false,
     					children: [{ text: "All WIP Reports", href: "/WIP", leaf: true}, { text: "Your WIP Items", href: "/YourWIP", leaf: true}] },
-  					{ text: 'Rota', leaf: false,
+  					{ text: 'Rota', id: 'rotaNode', leaf: false,
     					children: [{ text: "All Rotas", href: "/AllWIPReports", leaf: true}, { text: "Your Rota", href: "/YourWIP", leaf: true},
     					{ text: "Your Teams Rota", href: "/AllWIPReports", leaf: true} ]},
-  					{ text: 'Skillsets', leaf: false,
+  					{ text: 'Skillsets', id: 'skillsNode', leaf: false,
     					children: [{ text: "View Matrix", href: "/YourWIP", leaf: true}] }
 				]
 
 var rootNode = new Ext.tree.AsyncTreeNode({ text: 'Root', children: tree_data });
-var tree = new Ext.tree.TreePanel({ root: rootNode, border: false, rootVisible: false, stateful: true, stateId: "nav_tree" });
+var tree = new Ext.tree.TreePanel({ root: rootNode, border: false, plugins: [new Ext.ux.tree.State()], rootVisible: false, stateful: true, stateId: "nav_tree"});
+	
+    	
 var center_html_content = { xtype: "panel", contentEl: "center_panel_html", bodyStyle: "padding: 15px;"}
 var new_menu = { xtype: "tbbutton", text: "New", menu: [{ text: "New Project", href: "/Projects/New"}, { text: "New WIP Report", href: "/WIP/NEW" }]}
 var toolbar = new Ext.Toolbar({ items: [ new_menu ] });
@@ -19,7 +70,6 @@ var toolbar = new Ext.Toolbar({ items: [ new_menu ] });
 var center_panel = { xtype: "panel", region: "center", title: "WTMS Traffic", items: [ toolbar, center_html_content ], bodyStyle: "padding: 0px;"}
 var west_panel = { xtype: "panel", title: 'Navigation', region: 'west', margins: '0 0 0 0',
 					cmargins: '0 5 0 0', width: 175, minSize: 100, maxSize: 250, items: [ tree ] }
-
-
-
-
+					
+					
+					
