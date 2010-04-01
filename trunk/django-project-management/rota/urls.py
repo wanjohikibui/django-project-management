509aa1e0ddde3f712e4e4c5328431663de24fec1
mail@johnbullonline.com <mail@johnbullonline.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-04-01 16:44:45 +0000

    Allowing for single digit days (1-9) in rota.urls

--- trunk/django-project-management/rota/urls.py
+++ trunk/django-project-management/rota/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.views import *
 
 urlpatterns = patterns('rota.views',
-	(r'RotaItems/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'view_rota', { 'template': 'rota/ajax-allrota.html', 'pdf': False}),
+	(r'RotaItems/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'view_rota', { 'template': 'rota/ajax-allrota.html', 'pdf': False}),
 	(r'^All/$', 'rota_homepage'),
 	(r'^RotaActivities/$', 'view_rota_activities'),
 	(r'^RotaItems/$', 'view_rota'),
