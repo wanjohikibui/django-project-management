32058ecfc65c6882eabc00e42b6ebee17b2afde7
mozrat@gmail.com <mozrat@gmail.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-04-12 14:10:38 +0000

    Removing functionality that won't be in the released version

--- trunk/django-project-management/new_site_media/js/main-ui.js
+++ trunk/django-project-management/new_site_media/js/main-ui.js
@@ -55,9 +55,7 @@ var tree_data = [
   					{ text: 'Work In Progress', id: 'wipNode', leaf: false,
     					children: [{ text: "All WIP Reports", href: "/WIP/", leaf: true}] },
   					{ text: 'Rota', id: 'rotaNode', leaf: false,
-    					children: [{ text: "All Rotas", href: "/Rota/All/", leaf: true} ]},
-  					{ text: 'Skillsets', id: 'skillsNode', leaf: false,
-    					children: [{ text: "View Matrix", href: "/Matrix/", leaf: true}] }
+    					children: [{ text: "All Rotas", href: "/Rota/All/", leaf: true} ]}
 				]
 
 var rootNode = new Ext.tree.AsyncTreeNode({ text: 'Root', children: tree_data });
