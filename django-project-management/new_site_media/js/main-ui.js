97cad157a2cb705ced4ad377ccf4bd3d0bdf01f3
mozrat <mozrat@gmail.com>
2010-04-18 16:53:15 +0000

    Reduced the number of clicks required to get around the Traffic tool by using message boxes rather than clickable confirmation boxes

--- django-project-management/new_site_media/js/main-ui.js
+++ django-project-management/new_site_media/js/main-ui.js
@@ -87,7 +87,37 @@ var findLabel = function(field) {
     }    
 }  
 
+// Function for message passing boxes
 
+Ext.message = function(){
+    var msgCt;
+	function createBox(t, s){
+		// Added msg id
+        return ['<div class="msg" id="msg" name="msg">',
+                '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
+                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><h3>', t, '</h3>', s, '</div></div></div>',
+                '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
+                '</div>'].join('');
+    }  
+	return {
+	msg : function(title, format, sec){
+            if(!msgCt){
+                msgCt = Ext.DomHelper.insertFirst(document.body, {id:'msg-div'}, true);
+            }
+            
+            var s = String.format.apply(String, Array.prototype.slice.call(arguments, 1));
+            var m = Ext.DomHelper.append(msgCt, {html:createBox(title, s)}, true);
+            // Added The following code to get the <div> height
+            var yoffset = document.getElementById("msg").style.height;
+            // Then added that value to the yoffset position in alignTo 
+            msgCt.alignTo(document, 'br-br', [0,yoffset]);
+            m.slideIn('t').pause(sec).ghost("t", {remove:true});
+        }  
+    }
+}();
+
+
+// Navigation tree
 var tree_data = [
 					{ text: 'Projects', id: 'projectsNode', leaf: false, children: [{ text: 'View Dashboard', href: "/", leaf: true }]},
   					{ text: 'Work In Progress', id: 'wipNode', leaf: false, children: [{ text: "All WIP Reports", href: "/WIP/", leaf: true}] },
