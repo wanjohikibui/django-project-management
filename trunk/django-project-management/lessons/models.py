b66f68a3003e095f0ebde6343ee8e5ecb53082a8
mozrat@gmail.com <mozrat@gmail.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-05-06 17:49:03 +0000

    Code cleanup, Python tabs to spaces

--- trunk/django-project-management/lessons/models.py
+++ trunk/django-project-management/lessons/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class LessonLearnt(models.Model):
 
-	author = models.ForeignKey(User)
-	description = models.TextField(max_length=10240)
-	created_date = models.DateTimeField(auto_now_add=True)
-	modified_date = models.DateTimeField(auto_now=True)
-	publish_to_client = models.BooleanField(default=False)
+        author = models.ForeignKey(User)
+        description = models.TextField(max_length=10240)
+        created_date = models.DateTimeField(auto_now_add=True)
+        modified_date = models.DateTimeField(auto_now=True)
+        publish_to_client = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.description
+        def __str__(self):
+                return self.description
