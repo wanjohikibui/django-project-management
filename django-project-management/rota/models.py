ff843923d82c12f6a392228f4cd52e308a881db2
mozrat <mozrat@gmail.com>
2010-05-06 17:33:52 +0000

    Code cleanup, Python tabs to spaces

--- django-project-management/rota/models.py
+++ django-project-management/rota/models.py
@@ -5,45 +5,45 @@ from django.contrib.auth.models import User
 
 class RotaActivity(models.Model):
 
-	activity = models.CharField('Activity', max_length=255)
-	description = models.TextField('Description', blank=True)
-	unavailable_for_projects = models.BooleanField('Unavailable for Project', default=False)
-	active = models.BooleanField('Active', default=True)
+        activity = models.CharField('Activity', max_length=255)
+        description = models.TextField('Description', blank=True)
+        unavailable_for_projects = models.BooleanField('Unavailable for Project', default=False)
+        active = models.BooleanField('Active', default=True)
 
-	def __str__(self):
-		return '''%s''' % self.activity
+        def __str__(self):
+                return '''%s''' % self.activity
 
-	class Meta:
-		ordering = ('activity',)
+        class Meta:
+                ordering = ('activity',)
 
 class RotaItem(models.Model):
 
-	date = models.DateField('Date')
-	person = models.ForeignKey(User, related_name='rota_item')
-	activity = models.ForeignKey(RotaActivity, verbose_name='Activity', related_name='rota_items')
-	description = models.TextField('Description', blank=True)
+        date = models.DateField('Date')
+        person = models.ForeignKey(User, related_name='rota_item')
+        activity = models.ForeignKey(RotaActivity, verbose_name='Activity', related_name='rota_items')
+        description = models.TextField('Description', blank=True)
 
-	author = models.ForeignKey(User, related_name='rota_items_authored')
-	created_date = models.DateTimeField(auto_now_add=True)
-	modified_date = models.DateTimeField(auto_now=True)
+        author = models.ForeignKey(User, related_name='rota_items_authored')
+        created_date = models.DateTimeField(auto_now_add=True)
+        modified_date = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		permissions = (
-				('can_edit', 'Can Edit Rota'),
-		)
+        class Meta:
+                permissions = (
+                                ('can_edit', 'Can Edit Rota'),
+                )
 
-	def __str__(self):
-		return '''%s - %s - %s''' % ( self.date.strftime("%Y-%m-%d"), self.person.username, self.activity.activity )
+        def __str__(self):
+                return '''%s - %s - %s''' % ( self.date.strftime("%Y-%m-%d"), self.person.username, self.activity.activity )
 
-	
+        
 
 class Team(models.Model):
 
-	name = models.CharField('Name', max_length=255)	
-	members = models.ManyToManyField(User, verbose_name='Members', related_name='teams', blank=True, null=True)
-	
+        name = models.CharField('Name', max_length=255) 
+        members = models.ManyToManyField(User, verbose_name='Members', related_name='teams', blank=True, null=True)
+        
 
-	def __str__(self):
-		return '''%s''' % self.name
+        def __str__(self):
+                return '''%s''' % self.name
 
 
