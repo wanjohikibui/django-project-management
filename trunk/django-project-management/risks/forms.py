ba45b92330e774254d31a320754abf44bada6bf4
mozrat@gmail.com <mozrat@gmail.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-05-13 07:54:48 +0000

    Added a Update/History pair of fields to the Risk app

--- trunk/django-project-management/risks/forms.py
+++ trunk/django-project-management/risks/forms.py
@@ -2,12 +2,16 @@ from django.forms import *
 from django.contrib.admin import widgets                                       
 from projects.models import *
 from risks.models import *
-#from misc.widgets import DateTimeWidget
-#from backends.authlib import *
-
+from wbs.forms import WBSUpdateField
 
 class RiskForm(ModelForm):
         
-        class Meta:
-                model = Risk
-                fields = ('description', 'owner', 'probability', 'impact', 'counter_measure', 'status',)
+    class Meta:
+        model = Risk
+        fields = ('description', 'owner', 'probability', 'impact', 'counter_measure', 'status', 'history')
+    def __init__(self, *args, **kwargs):
+        super(RiskForm, self).__init__(*args, **kwargs)
+        self.fields['history'].widget.attrs['readonly'] = True
+        self.fields['update'] = WBSUpdateField()
+        self.fields['update'].widget = Textarea()
+        self.fields['update'].label = 'Update'
