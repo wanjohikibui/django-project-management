74647b6465a81963bf7276573479fccceeeb5c2c
mozrat@gmail.com <mozrat@gmail.com@c2038948-bc8a-11de-9a73-11f8458f6c97>
2010-10-11 17:30:13 +0000

    Implemented the following REST API URLs
    GET /api/risks # Get a list of the users assigned risks
    
    GET /api/risks/PRJ1234/ # Get a list of all risks associated with a project
    POST /api/risks/PRJ1234/ # Add a new risk to the project
    
    GET /api/risks/PRJ1234/RISK-SM-1234/ # Get an individual risk
    PUT /api/risks/PRJ1234/RISK-SM-1234/ # Update an individual risk
    DELETE /api/risks/PRJ1234/RISK-SM-1234/ # Delete an individual risk

--- branches/rest-api-branch/django-project-management/api/urls.py
+++ branches/rest-api-branch/django-project-management/api/urls.py
@@ -3,11 +3,13 @@ from piston.authentication import HttpBasicAuthentication
 from piston.resource import Resource
 from projects.api_views import ProjectResourceHandler, ProjectListHandler, CompanyListHandler, \
         TeamManagersListHandler, NonTeamManagersListHandler, ResourcesListHandler
+from risks.api_views import RiskResourceHandler, RiskListHandler, \
+        UserRiskListHandler
 
 auth = HttpBasicAuthentication(realm="My Realm")
 ad = { 'authentication': auth }
 
-
+# Handlers defined in projects.api_views
 team_managers_handler = Resource(TeamManagersListHandler, **ad)
 non_team_managers_handler = Resource(NonTeamManagersListHandler, **ad)
 resources_handler = Resource(ResourcesListHandler, **ad)
@@ -15,6 +17,10 @@ project_handler = Resource(ProjectResourceHandler, **ad)
 project_list_handler = Resource(ProjectListHandler, **ad)
 company_list_handler = Resource(CompanyListHandler, **ad)
 
+# Handlers defined in risks.api_views
+risk_handler = Resource(RiskResourceHandler, **ad)
+risk_list_handler = Resource(RiskListHandler, **ad)
+user_risk_list_handler = Resource(UserRiskListHandler, **ad)
 
 urlpatterns = patterns('',
     # URLs handled in projects.api_views
@@ -25,5 +31,9 @@ urlpatterns = patterns('',
     (r'projects/$', project_list_handler),
     (r'companies/$', company_list_handler),
 
+    # URLs handled in risks.api_views
+    (r'risks/(?P<project_number>[-\w\./\s]+)/(?P<risk_number>[-\w\./\s]+)/$', risk_handler),
+    (r'risks/(?P<project_number>[-\w\./\s]+)/$', risk_list_handler),
+    (r'risks/$', user_risk_list_handler),
 
 )
